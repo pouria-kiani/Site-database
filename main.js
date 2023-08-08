d26112c4fc6819ef9dd30e6e9e4c72c7011015d6
pouria kiani <pouria.kiani7@gmail.com>
2023-08-08 18:59:36 +0330

    making cpu objects

---
+++ main.js
@@ -0,0 +1,63 @@
+// ? computerPart & accessories
+let monitors = [
+  {
+    name: "benQ",
+    model: "GW2780",
+    size: "27-inch",
+    price: "7960000",
+  },
+  {
+    name: "benQ",
+    model: "XL2740",
+    size: "27-inch",
+    price: "20300000",
+  },
+  {
+    name: "benQ",
+    model: "EW2480",
+    size: "23.8-inch",
+    price: "8110000",
+  },
+  {
+    name: "benQ",
+    model: "EW3280U",
+    size: "32-inch",
+    price: "30600000",
+  },
+  {
+    name: "asus",
+    model: "VZ24EHE",
+    size: "24-inch",
+    price: "6200000",
+  },
+  {
+    name: "asus",
+    model: "VP228HI",
+    size: "21.5-inch",
+    price: "unavaible",
+  },
+  {
+    name: "SAMSUNG",
+    model: "S22A330",
+    size: "22-inch",
+    price: "3660000",
+  },
+  {
+    name: "SAMSUNG",
+    model: "C310",
+    size: "24-inch",
+    price: "6900000",
+  },
+  {
+    name: "Gplus",
+    model: "GDM-245LN",
+    size: "24-inch",
+    price: "4860000",
+  },
+  {
+    name: "Gplus",
+    model: "GDM-226LN",
+    size: "22-inch",
+    price: "4380000",
+  },
+];
